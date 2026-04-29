c41d440523d2e14bdc5da5a7698e018b93f1afd9
dojo <djdumaluan.prog@gmail.com>
2026-04-30 02:47:45 +0800

    chore: defined supabase url & service role key envs

--- backend/lib/env.js
+++ backend/lib/env.js
@@ -4,6 +4,8 @@ const ENV = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   AUTH_TOKEN_SECRET: process.env.AUTH_TOKEN_SECRET,
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
 };
 
 export { ENV };
