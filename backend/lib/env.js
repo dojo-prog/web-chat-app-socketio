a7ea8b3f4a318d963fc7765a8f99aad07a084fd2
dojo <djdumaluan.prog@gmail.com>
2026-04-30 01:32:59 +0800

    chore: set auth secret env

--- backend/lib/env.js
+++ backend/lib/env.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 const ENV = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
+  AUTH_TOKEN_SECRET: process.env.AUTH_TOKEN_SECRET,
 };
 
 export { ENV };
