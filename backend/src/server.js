d10ebf196c94188ad65eadf00b46717744f8daca
dojo <djdumaluan.prog@gmail.com>
2026-04-29 16:58:52 +0800

    chore: applied data parsers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { ENV } from "../lib/env.js";
 import testConnection from "../db/test.js";
 
 const app = express();
 const PORT = ENV.PORT;
+
+app.use(express.json({ limit: "2mb" }));
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
