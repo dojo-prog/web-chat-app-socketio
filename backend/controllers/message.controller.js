28d830fdc2f70604641dbd12101ab92924e13cc4
dojo <djdumaluan.prog@gmail.com>
2026-04-30 16:49:18 +0800

    feat: defined getUserContactList

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -23,6 +23,50 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
-export const getUserContactList = async (req, res) => {};
+export const getUserContactList = async (req, res) => {
+  const user = req.user;
+
+  try {
+    let result;
+
+    result = await db.query(
+      `
+      SELECT senderId, receiverId FROM messages
+      WHERE senderId = $1 OR receiverId = $1
+      `,
+      [user.id],
+    );
+
+    const contactIds = result.rows.flatMap((row) => [
+      row.senderId,
+      row.receiverId,
+    ]);
+    const uniqueIds = [...new Set(contactIds)];
+
+    if (uniqueIds.length === 0) {
+      return res.status(200).json({ contacts: [] });
+    }
+
+    const placeholders = uniqueIds.map((_, i) => `$${i + 1}`).join(", ");
+
+    result = await db.query(
+      `
+      SELECT fname, lname, email, avatar_url
+      FROM users
+      WHERE id IN (${placeholders})
+      ORDER BY fname ASC;
+      `,
+      uniqueIds,
+    );
+
+    const contacts = result.rows;
+
+    res.status(200).json({ contacts });
+  } catch (error) {
+    console.error("getUserContactList controller error:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
 export const getMessagesByUserId = async (req, res) => {};
 export const sendMessage = async (req, res) => {};
