62bc1208d9d65e2c1b32dc4dfbe70a938a3fe421
dojo <djdumaluan.prog@gmail.com>
2026-05-01 19:10:59 +0800

    chore: initialized auth layout

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="h-[80%] max-w-4xl w-full border border-gray-200 rounded-lg shadow overflow-hidden">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
