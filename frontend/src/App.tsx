277a3c7e1a57ea6df9cd7dad1b3f5d55f7dddbec
dojo <djdumaluan.prog@gmail.com>
2026-05-01 18:35:21 +0800

    chore: scaffold application routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import ChatPage from "./pages/ChatPage";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 const App = () => {
-  return <div className="text-7xl">App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<ChatPage />} />
+        </Route>
+
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
