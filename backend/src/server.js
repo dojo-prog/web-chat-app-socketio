2a9f36760d6ede9387fc5b3b7cf9ab7524068abf
dojo <djdumaluan.prog@gmail.com>
2026-05-02 17:37:42 +0800

    chore: defined cors config on app

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,11 @@ import testConnection from "../db/test.js";
 import { testStorage } from "../lib/supabase.js";
 import authRouter from "../routes/auth.route.js";
 import { app, server } from "../lib/socket.js";
+import cors from "cors";
 
 const PORT = ENV.PORT;
+
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
 app.use(express.json({ limit: "2mb" }));
 app.use(cookieParser());
