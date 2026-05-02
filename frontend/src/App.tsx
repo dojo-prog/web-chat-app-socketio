3e5a0a801a187052a92fa77d314258a7af42915b
dojo <djdumaluan.prog@gmail.com>
2026-05-02 17:41:00 +0800

    chore: guarded pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import ChatPage from "./pages/ChatPage";
 import AuthLayout from "./layouts/AuthLayout";
@@ -6,16 +6,25 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import { Slide, ToastContainer } from "react-toastify";
+import useAuthStore from "./stores/auth.store";
 
 const App = () => {
+  const { user } = useAuthStore();
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
+        <Route
+          path="/"
+          element={user ? <MainLayout /> : <Navigate to={"/auth"} />}
+        >
           <Route index element={<ChatPage />} />
         </Route>
 
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route
+          path="/auth"
+          element={!user ? <AuthLayout /> : <Navigate to={"/"} />}
+        >
           <Route index element={<LoginPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignupPage />} />
