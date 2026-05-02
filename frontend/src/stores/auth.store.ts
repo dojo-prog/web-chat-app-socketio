a9ce929a96ef8643e87d3ae086b61c69e9a663e0
dojo <djdumaluan.prog@gmail.com>
2026-05-02 17:24:22 +0800

    feat: defined auth store actions

--- frontend/src/stores/auth.store.ts
+++ frontend/src/stores/auth.store.ts
@@ -1,6 +1,11 @@
 import { create } from "zustand";
+import axios from "../lib/axios";
+import { toast } from "react-toastify";
+import { returnMissing } from "../utils/returnMissing";
+import { validateSignin, validateSignup } from "../validators/auth.validator";
+import { capitalize } from "../utils/capitalize";
 
-interface User {
+export interface User {
   id: string;
   fname: string;
   lname: string;
@@ -10,16 +15,16 @@ interface User {
   created_at: string;
 }
 
-interface UserWithImage extends User {
+export interface UserWithImage extends User {
   image: File;
 }
 
-interface SigninInputs {
+export interface SigninInputs {
   email: string;
   password: string;
 }
 
-interface SignupInputs extends SigninInputs {
+export interface SignupInputs extends SigninInputs {
   fname: string;
   lname: string;
   cPassword: string;
@@ -44,11 +49,140 @@ const useAuthStore = create<AuthState>((set) => ({
   checkingAuth: false,
   loading: false,
 
-  checkAuth: async () => {},
-  signup: async () => {},
-  login: async () => {},
-  logout: async () => {},
-  updateUserProfile: async () => {},
+  checkAuth: async () => {
+    set({ checkingAuth: true });
+
+    try {
+      const res = await axios.get("/auth/profile");
+      set({ user: res.data.user ?? null });
+    } catch (error) {
+      console.error("checkAuth error:", error);
+    } finally {
+      set({ checkingAuth: false });
+    }
+  },
+
+  signup: async (inputs) => {
+    const { fname, lname, email, password, cPassword } = inputs;
+    const { error } = validateSignup(inputs);
+
+    if (error) {
+      toast.error(error);
+      return;
+    }
+
+    const n = {
+      fname: capitalize(fname),
+      lname: capitalize(lname),
+      email: email.toLowerCase().trim(),
+    };
+
+    set({ loading: true });
+    try {
+      const res = await axios.post("/auth/signup", {
+        password,
+        cPassword,
+        ...n,
+      });
+      set({ user: res.data.user ?? null });
+      toast.success(res.data.message || "Signup successful");
+    } catch (error: any) {
+      console.error("singup error:", error);
+      toast.error(
+        error.response.data.message || "Error signing you up. Please try again",
+      );
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  login: async (inputs) => {
+    const { email, password } = inputs;
+    const { error } = validateSignin(inputs);
+
+    if (error) {
+      toast.error(error);
+      return;
+    }
+
+    set({ loading: true });
+    try {
+      const res = await axios.post("/auth/login", {
+        email: email.toLowerCase().trim(),
+        password,
+      });
+      set({ user: res.data.user ?? null });
+      toast.success(res.data.message || "Login successful");
+    } catch (error: any) {
+      console.error("login error:", error);
+      toast.error(
+        error.response.data.message || "Error logging you in. Please try again",
+      );
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axios.post("auth/logout");
+      toast.success("Logout successful");
+    } catch (error: any) {
+      console.error("logout error:", error);
+      toast.error(
+        error.response.data.message ||
+          "Error while logging you out. Please try again",
+      );
+    }
+  },
+
+  updateUserProfile: async (original, modified) => {
+    if (!original) {
+      console.error("Failed to pass in user details");
+      return;
+    }
+
+    if (!modified) {
+      console.error("Failed to pass in changes");
+      return;
+    }
+
+    const changes: Partial<User> = {};
+
+    (["fname", "lname"] as (keyof User)[]).forEach((key) => {
+      if (modified[key]?.toLowerCase() !== original[key]?.toLowerCase()) {
+        changes[key] = capitalize(modified[key]!);
+      }
+    });
+
+    if (Object.keys(changes).length === 0 && !modified.image) {
+      toast.error("No changes has been made");
+      return;
+    }
+
+    const nModified = {
+      fname: capitalize(modified.fname),
+      lname: capitalize(modified.lname),
+    };
+
+    set({ loading: true });
+    try {
+      const res = await axios.put("/auth/update-profile", {
+        original,
+        modified: { ...modified, ...nModified },
+      });
+      set({ user: res.data.user ?? null });
+      toast.success(res.data.message ?? "Profile updated");
+    } catch (error: any) {
+      console.error("updateUserProfile error:", error);
+      toast.error(
+        error.response.data.message ??
+          "Error while updating your profile. Please try again",
+      );
+    } finally {
+      set({ loading: false });
+    }
+  },
 }));
 
 export default useAuthStore;
