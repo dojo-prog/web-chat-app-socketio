7cd94366331459f605673932d24dd3c7a336e5a0
dojo <djdumaluan.prog@gmail.com>
2026-05-05 20:13:06 +0800

    feat: created profile page ui

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,5 +1,86 @@
+import { PenIcon } from "lucide-react";
+import CustomButton from "../components/CustomButton";
+import CustomInput from "../components/CustomInput";
+import { useForm } from "../hooks/useForm";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { formData, handleChange } = useForm({
+    fname: "",
+    lname: "",
+    image: "",
+  });
+
+  return (
+    <div className="h-[calc(100vh-9.5rem)] w-full flex justify-center">
+      <div className="h-full max-w-xl w-full bg-white rounded-lg p-12 flex flex-col">
+        {/* Header */}
+        <div className="flex justify-between mb-4">
+          <h2 className="text-4xl font-bold">Edit profile</h2>
+          <div className="relative w-30 h-30 rounded-full bg-blue-100 ">
+            <img
+              src=""
+              alt=""
+              className="h-full w-full rounded-full object-cover"
+            />
+
+            <input
+              type="file"
+              accept="image/*"
+              className="hidden"
+              id="image"
+              name="image"
+              onChange={(e) => {
+                // handleFileChange(e);
+                // const file = e.target.files?.[0];
+                // if (!file) return;
+                // setImagePreview(URL.createObjectURL(file));
+              }}
+            />
+
+            <label
+              htmlFor="image"
+              className="absolute bottom-1 left-3 h-6 w-6 rounded-full border-2 border-blue-500 bg-white flex items-center justify-center p-1 cursor-pointer"
+            >
+              <PenIcon className="h-full text-blue-500" />
+            </label>
+          </div>
+        </div>
+
+        {/* User Fields */}
+        <div className="h-full flex flex-col">
+          <div className="flex-1 space-y-4">
+            <div className="flex items-center justify-between space-x-4">
+              <CustomInput
+                label="First Name"
+                id="fname"
+                value={formData.fname}
+                onChange={handleChange}
+              />
+              <CustomInput
+                label="Last Name"
+                id="lname"
+                value={formData.lname}
+                onChange={handleChange}
+              />
+            </div>
+
+            <CustomInput
+              label="Email (cannot modify)"
+              id="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              disabled={true}
+            />
+          </div>
+
+          <div className="flex items-center spacex-4">
+            <CustomButton type="button" title="Save" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
