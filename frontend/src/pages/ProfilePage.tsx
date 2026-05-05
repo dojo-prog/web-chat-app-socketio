481db38124668b3e4eb62793944cf3017d7c393c
dojo <djdumaluan.prog@gmail.com>
2026-05-05 20:30:02 +0800

    feat: applied profile update functionality

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,14 +1,37 @@
-import { PenIcon } from "lucide-react";
+import { PenIcon, UserIcon } from "lucide-react";
 import CustomButton from "../components/CustomButton";
 import CustomInput from "../components/CustomInput";
 import { useForm } from "../hooks/useForm";
+import useAuthStore, { type UserWithImage } from "../stores/auth.store";
+import { useEffect, useState } from "react";
 
 const ProfilePage = () => {
-  const { formData, handleChange } = useForm({
+  const { user, updateUserProfile, loading } = useAuthStore();
+
+  const { formData, setFormData, handleChange, handleFileChange } = useForm({
     fname: "",
     lname: "",
+    email: "",
     image: "",
   });
+
+  const [imagePreview, setImagePreview] = useState("");
+
+  useEffect(() => {
+    if (!user) return;
+    const { fname, lname, email } = user;
+    setFormData({
+      fname,
+      lname,
+      email,
+    });
+  }, []);
+
+  const handleSubmit = () => {
+    if (!user) return;
+
+    updateUserProfile(user, formData as UserWithImage);
+  };
 
   return (
     <div className="h-[calc(100vh-9.5rem)] w-full flex justify-center">
@@ -16,12 +39,22 @@ const ProfilePage = () => {
         {/* Header */}
         <div className="flex justify-between mb-4">
           <h2 className="text-4xl font-bold">Edit profile</h2>
-          <div className="relative w-30 h-30 rounded-full bg-blue-100 ">
-            <img
-              src=""
-              alt=""
-              className="h-full w-full rounded-full object-cover"
-            />
+          <div className="relative w-30 h-30 rounded-full bg-blue-100 flex items-center justify-center">
+            {user?.avatar_url ? (
+              <img
+                src={user.avatar_url}
+                alt={user.fname}
+                className="h-full w-full rounded-full object-cover"
+              />
+            ) : imagePreview ? (
+              <img
+                src={imagePreview}
+                alt="preview"
+                className="h-full w-full rounded-full object-cover"
+              />
+            ) : (
+              <UserIcon className="h-16 w-16 text-blue-500" />
+            )}
 
             <input
               type="file"
@@ -30,10 +63,10 @@ const ProfilePage = () => {
               id="image"
               name="image"
               onChange={(e) => {
-                // handleFileChange(e);
-                // const file = e.target.files?.[0];
-                // if (!file) return;
-                // setImagePreview(URL.createObjectURL(file));
+                handleFileChange(e);
+                const file = e.target.files?.[0];
+                if (!file) return;
+                setImagePreview(URL.createObjectURL(file));
               }}
             />
 
@@ -75,7 +108,13 @@ const ProfilePage = () => {
           </div>
 
           <div className="flex items-center spacex-4">
-            <CustomButton type="button" title="Save" />
+            <CustomButton
+              type="button"
+              title="Save"
+              onClick={handleSubmit}
+              loading={loading}
+              disabled={loading}
+            />
           </div>
         </div>
       </div>
