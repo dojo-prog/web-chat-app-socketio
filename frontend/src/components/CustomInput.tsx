c0d9fa66188648a334619b94d92a45d8ff0a4f05
dojo <djdumaluan.prog@gmail.com>
2026-05-05 08:39:38 +0800

    refactor: applied forwardRef to CustomInputs to allow utilization of ref feature

--- frontend/src/components/CustomInput.tsx
+++ frontend/src/components/CustomInput.tsx
@@ -1,4 +1,4 @@
-import type { ChangeEvent } from "react";
+import { forwardRef, type ChangeEvent } from "react";
 
 interface CustomInputProps {
   label?: string;
@@ -14,41 +14,47 @@ interface CustomInputProps {
   inputStyles?: string;
 }
 
-const CustomInput = ({
-  label,
-  type = "text",
-  placeholder = "Type here...",
-  id,
-  value,
-  onChange,
-  disabled = false,
-  required = true,
-  autocomplete = true,
-  labelStyles,
-  inputStyles,
-}: CustomInputProps) => {
-  return (
-    <div>
-      {label && (
-        <label htmlFor={id} className={`text-sm capitalize ${labelStyles}`}>
-          {label}{" "}
-          {required && <span className="text-bold text-red-500">*</span>}
-        </label>
-      )}
-      <input
-        type={type}
-        placeholder={placeholder}
-        id={id}
-        name={id}
-        value={value}
-        onChange={onChange}
-        disabled={disabled}
-        required={required}
-        autoComplete={autocomplete ? "on" : "off"}
-        className={`w-full border border-gray-200 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${inputStyles}`}
-      />
-    </div>
-  );
-};
+const CustomInput = forwardRef<HTMLInputElement, CustomInputProps>(
+  (
+    {
+      label,
+      type = "text",
+      placeholder = "Type here...",
+      id,
+      value,
+      onChange,
+      disabled = false,
+      required = true,
+      autocomplete = true,
+      labelStyles,
+      inputStyles,
+    },
+    ref,
+  ) => {
+    return (
+      <div>
+        {label && (
+          <label htmlFor={id} className={`text-sm capitalize ${labelStyles}`}>
+            {label}{" "}
+            {required && <span className="text-bold text-red-500">*</span>}
+          </label>
+        )}
+        <input
+          type={type}
+          placeholder={placeholder}
+          ref={ref}
+          id={id}
+          name={id}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+          required={required}
+          autoComplete={autocomplete ? "on" : "off"}
+          className={`w-full border border-gray-200 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${inputStyles}`}
+        />
+      </div>
+    );
+  },
+);
 
 export default CustomInput;
